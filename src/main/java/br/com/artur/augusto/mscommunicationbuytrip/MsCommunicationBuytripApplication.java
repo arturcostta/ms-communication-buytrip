0a9b53f23dd23e8d8e5b31a42fb4ad341c49d5d8
Augusto Artur <arturcostta@gmail.com>
2020-02-06 19:59:05 -0300

    Commit inicial

---
+++ src/main/java/br/com/artur/augusto/mscommunicationbuytrip/MsCommunicationBuytripApplication.java
@@ -0,0 +1,13 @@
+package br.com.artur.augusto.mscommunicationbuytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCommunicationBuytripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCommunicationBuytripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/artur/augusto/mscommunicationbuytrip/MsCommunicationBuytripApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.artur.augusto.mscommunicationbuytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCommunicationBuytripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
